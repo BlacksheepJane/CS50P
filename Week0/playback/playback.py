9c3cb5c581019a686a56815c9367e8f9cffb688f
Jane <jialin.jane@sjtu.edu.cn>
2023-08-07 16:37:20 +0800

    complete playback

---
+++ Week0/playback/playback.py
@@ -0,0 +1,13 @@
+print("...".join(input().split()))
+
+# 'sep'.join(sep_object)
+# 用新sep隔开sep_object
+# str.split(str="",num=string.count(str))[n]
+# 读到str断开（默认为空格），不含str，一直断num个，[n]:表示选取第n个分片
+
+
+# mine
+# for c in input():
+#    if c == ' ': print("...",end='')
+#    else: print(c,end='')
+# print()
