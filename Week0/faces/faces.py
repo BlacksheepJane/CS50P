9837050809f7493491f5719e08530f2e46fb0301
Jane <jialin.jane@sjtu.edu.cn>
2023-08-07 17:25:16 +0800

    complete faces

---
+++ Week0/faces/faces.py
@@ -0,0 +1,5 @@
+# mine
+# print("🙁".join("🙂".join(input().split(":)")).split(":(")))
+
+print(input().replace(":)", "🙂").replace(":(", "🙁"))
+# str.replace(old, new[, max])  max -- 可选字符串, 替换不超过 max 次
